7c82e588b61a344337b2c41fdd057443e60329be
Raiden Dilan <Raiden@Raidens-MacBook.home>
2016-10-03 21:43:21 +0100

    game logic

---
+++ main.js
@@ -0,0 +1,15 @@
+// Represents the cards if the game
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour  = "king";
+
+// Checks for equality between two cards
+if (cardOne == cardTwo) {
+  alert("Yay! A Match!");
+}
+
+if (cardOne == cardFour) {
+	alert("Yay! A Match!");
+}
