07967ca1734dc7af362835c901966dbf22a2369c
RaidenDilan <Raiden@Raidens-MacBook.local>
2017-01-20 01:33:12 +0000

    final unit assignment

--- main.js
+++ main.js
@@ -1,26 +1,56 @@
-// Represents the cards if the game
+// Represents the cards of the game.
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour  = "king";
+// Represents the cards in play.
+var cardsInPlay = [];
 
-// Checks for equality between two cards
-/*if (cardOne === cardTwo) {
-  alert("Yay! A Match!");
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  // Loop through your cards array.
+  for (var i=0; i<cards.length; i++) {
+    // Creates a div element which will be used as a card.
+    var cardElement = document.createElement('div');
+    // Add a class to the card element to help link styling.
+    cardElement.className = 'card';
+	// This will set the card's 'data-card'.
+    cardElement.setAttribute('data-card', cards[i]);
+	// When any card is clicked the function isTwoCards will be activated.
+    cardElement.addEventListener('click', isTwoCards);
+
+    // Appends the card to the board
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+}
+// Checks to see if there are any cards in play.
+function isTwoCards() {
+  // Adds a card to array of cards being viewed/clicked.
+  cardsInPlay.push(this.getAttribute('data-card'));
+	// Shows the card's image when clicked.
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='images/king.png'>"; // King image
+	} else {
+		this.innerHTML = "<img src='images/queen.png'>"; //Queen image
+	}
+  // Checks for a match if you have two cards in play/clicked.
+  if (cardsInPlay.length === 2) {
+    // Passes the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // Clears cards in play array for next try
+    cardsInPlay = [];
+  }
+}
+function isMatch(cards) {
+  // Alerts the viewer with a winning message
+  if (cards[0] === cards[1]) {
+    alert("Congradulations!");
+  } else {
+    alert("Please try again!");
+
+  }
 }
 
-if (cardOne === cardFour) {
-	alert("Yay! A Match!");
-}*/
-
-var gameBoard = document.getElementById("game-board");
-
-var createCards = function() {
-  for (var i = 0; i < 4; i++) {
-    var newCards = document.createElement("div");
-    newCards.className = "card";
-    gameBoard.appendChild(newCards);
-  }
-};
-createCards();
+createBoard();
